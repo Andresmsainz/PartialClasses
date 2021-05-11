2140833d3e03e7dc8b07b9f00b9f1398d2672cf2
Andres Sainz <andresmsainz@gmail.com>
2021-05-10 22:17:07 -0400

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PartialClasses
+{
+    partial class Animals
+    {
+        public string animalColor;
+        public string animalName;
+
+        public void GetName()
+        {
+            Console.WriteLine("My name is Max the Dog");
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PartialClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Partial Classes!");
+            Animals animal = new Animals();
+
+            //animal now holds properties and methods from both partial classes in Class1 & Class2
+            animal.GetName();
+        }
+    }
+}
